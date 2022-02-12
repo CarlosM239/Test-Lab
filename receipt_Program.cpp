d810786020cfff1bf47e23703c4dd04b18b5674f
CarlosM239 <99521449+CarlosM239@users.noreply.github.com>
2022-02-11 22:45:22 -0500

    Update receipt_Program.cpp

--- receipt_Program.cpp
+++ receipt_Program.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <array>
-
-
-using std::array;
-using std::vector;
-using std::cin;
-using std::string;
-using std::endl;
-using std::cout;
-using std::printf;
+#include "receipt_Program.h"
